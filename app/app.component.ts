fba81aa58f394aabf47600f9aef9dbd77ee2dc32
shawnfc <shawn@shawnconrad.com>
2016-06-09 18:53:33 -0400

    added files

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My first Angular 2 App</h1>',
+    directives: [AppComponent]
+})
+
+export class AppComponent {
+
+}
