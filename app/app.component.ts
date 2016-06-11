0b1b8435407a659903537a6c972511a92e617e43
shawnfc <shawn@shawnconrad.com>
2016-06-11 14:30:40 -0400

    Completed First Project

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,6 @@
-import {Component} from '@angular/core';
-
+import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My first Angular 2 App</h1>',
-    directives: [AppComponent]
+    template: '<h1>My Second Angular 2 App</h1>'
 })
-
-export class AppComponent {
-
-}
+export class AppComponent { }
