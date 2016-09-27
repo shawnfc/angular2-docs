60ea80006f15c76194ab9cdd783af5415cfd8c0a
Shawn <mrshawnconrad@gmail.com>
2016-09-27 17:02:30 +0000

    working with Angular 2 Final

---
+++ angular-quickstart/app/app.module.ts
@@ -0,0 +1,11 @@
+//Entry point of the application
+
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/core';
+
+@NgModule({
+    import: [BrowserModule]
+})
+
+export class AppModule { }
+
