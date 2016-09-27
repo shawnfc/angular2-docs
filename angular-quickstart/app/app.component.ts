9d4625501c2a4a167a36e91b2811ab464e3e2dae
shawnfc <shawn@shawnconrad.com>
2016-09-27 15:53:24 -0400

    completed quickstart example

---
+++ angular-quickstart/app/app.component.ts
@@ -0,0 +1,10 @@
+// Root Component
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My SECOND Angular App</h1>'
+})
+
+export class AppComponent {}
